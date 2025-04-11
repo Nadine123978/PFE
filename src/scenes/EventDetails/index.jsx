684cc63b6b419979f12823c14c3fbbd646a8bf3c
Israa129 <israakhatibl2@gmail.com>
2025-04-11 15:12:47 +0300

    sjsjjs

--- src/scenes/EventDetails/index.jsx
+++ src/scenes/EventDetails/index.jsx
@@ -1,31 +1,4 @@
 import { useParams } from "react-router-dom";
-<<<<<<< HEAD
-import { Box, useTheme,Typography, Button } from "@mui/material";
-import { tokens } from "../../theme";
-import { useState } from "react";
-import Header from "../../components/Header";
-import EventPoster from "../../components/EventPoster";
-
-const EventDetails = () => {
-  const { id } = useParams();  // استخدم useParams للحصول على معرّف الحدث من الرابط
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
-
-    return (
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          ml: "250px",
-          width: "calc(100vw - 250px)",
-        }}
-      >
-        <Header title="DEtails Events" subtitle="Dashboard / Events /Details" />
-
-        <EventPoster />
-     </Box>
-=======
 import { Box, Stack, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 import Header from "../../components/Header";
@@ -69,7 +42,6 @@ const EventDetails = () => {
         </Box>
       </Box>
     </Box>
->>>>>>> 3b4e9ed (Add AdventureCard component with design and Book Now button)
   );
 };
 
